60212cba4fb3b8f90087ee544aac60049006cd37
LEAL <alejandrolealdev@gmail.com>
2025-07-29 13:50:31 +0200

    Sistema de hambre #1

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 
+# Referencias a nodos de UI
+@onready var hunger_bar := get_node("../UI/HungerBar")
+@onready var feed_button := get_node("../UI/FeedButton")
+
+# Sistema de movimiento
 var speed = 100 #Velocidad normal
 var direction: int = -1 #Direccion: 1 = derecha, -1 = izquierda
 var gravity: float = 600.0 #Gravedad
-var jump_force: float = 300.0 # Fuerza del salto
+var jump_force: float = 300.0 #Fuerza del salto
+
+# Sistema de hambre
+var hunger: int = 100 #Hambre total
+var hunger_decrease_rate: int = 5 #Ratio de decrecimiento
+var feed_amount: int = 20 #Cantidad de alimento
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("walk")
@@ -28,18 +38,33 @@ func _physics_process(delta):
 	var screen_size = get_viewport_rect().size #Reconoce el tamaño de la pantalla y lo convierte en Vector2
 	var position_in_screen = global_position.x
 
-	if position_in_screen <= 0:
+	if position_in_screen <= 100:
 		direction = 1
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.flip_h = true
 
-	elif position_in_screen >= screen_size.x:
+	elif position_in_screen >= screen_size.x -100:
 		direction = -1
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.flip_h = false
 
+# Funcion integrada que se ejecuta en cada frame. DELTA: según el valor del tiempo no del frame
+func _process(delta: float) -> void:
+	# Reducir hambre con el tiempo
+	hunger -= hunger_decrease_rate * delta
+	hunger = clamp(hunger, 0, 100)
+	#hunger_bar.value = hunger
 
+	if hunger == 0:
+		print("¡Tu Tamagotchi tiene hambre!")
+		# Aquí puedes poner animación de tristeza o Game Over
 
+# Funcion _on_<NombreDelNodo>_<NombreDeLaSeñal>()
+func _on_feed_pressed() -> void:
+	hunger += feed_amount
+	hunger = clamp(hunger, 0, 100)
+	#hunger_bar.value = hunger
+	print("¡Alimentado! Hunger ahora:", hunger)
 
 func _on_area_entered(area: CharacterBody2D) -> void:
 	print("choque con un enemigo")
