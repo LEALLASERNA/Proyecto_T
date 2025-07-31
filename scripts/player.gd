dc0bb4f301c7d9435c8ae0f535ec7f3c9f44cbb4
LEAL <alejandrolealdev@gmail.com>
2025-07-31 15:06:05 +0200

    x

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,23 +1,14 @@
 extends CharacterBody2D
 
-# Referencias a nodos de UI
-@onready var hunger_bar := get_tree().get_current_scene().get_node("HungerBar")
-
 # Sistema de movimiento
 var speed: float = 100.0
 var direction: int = -1 # Dirección inicial (izquierda)
 var gravity: float = 600.0
 var jump_force: float = 300.0
 
-# Sistema de hambre
-var hunger: float = 100.0
-var max_hunger: float = 100.0
-var hunger_decrease_rate: float = 10.0  # Cantidad por segundo
 
 func _ready() -> void:
-	print("Buscando barra de hambre:", hunger_bar)
 	$AnimatedSprite2D.play("walk")
-	update_hunger_bar()
 
 func _physics_process(delta: float) -> void:
 	# Movimiento vertical (salto y gravedad)
@@ -44,20 +35,3 @@ func _physics_process(delta: float) -> void:
 		direction = -1
 		$AnimatedSprite2D.play("walk")
 		$AnimatedSprite2D.flip_h = false
-
-func _process(delta: float) -> void:
-	# Disminuir hambre con el tiempo
-	hunger -= hunger_decrease_rate * delta
-	hunger = clamp(hunger, 0, max_hunger)
-	update_hunger_bar()
-
-	if hunger == 0:
-		print("¡Tu Tamagotchi tiene hambre!")
-		# Aquí puedes añadir animación de tristeza, sonido, etc.
-
-func update_hunger_bar() -> void:
-	if hunger_bar:
-		print("Actualizando barra con hambre:", hunger)
-		hunger_bar.set_hunger(hunger)
-	else:
-		print("NO SE ENCONTRÓ el nodo HungerBar")
